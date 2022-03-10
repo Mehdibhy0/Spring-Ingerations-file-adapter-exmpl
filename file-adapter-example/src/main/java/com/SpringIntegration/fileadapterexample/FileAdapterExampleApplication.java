fa84d883f104313b2a41cbea76f1cf3352df8642
Mehdibhy0 <96703308+Mehdibhy0@users.noreply.github.com>
2022-03-10 10:20:22 +0100

    Add files via upload

---
+++ file-adapter-example/src/main/java/com/SpringIntegration/fileadapterexample/FileAdapterExampleApplication.java
@@ -0,0 +1,13 @@
+package com.SpringIntegration.fileadapterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAdapterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAdapterExampleApplication.class, args);
+	}
+
+}
---
+++ file-adapter-example/src/test/java/com/SpringIntegration/fileadapterexample/FileAdapterExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringIntegration.fileadapterexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAdapterExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
